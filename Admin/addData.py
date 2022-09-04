6ea68f5ad842dc03ab4a4a208d4934240f0f6c20
Wakhid Kurniawan <kurniawan.wk48@gmail.com>
2022-09-04 13:38:04 +0700

    adding files

---
+++ Admin/addData.py
@@ -0,0 +1,35 @@
+import unittest
+import time
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class TestLogin(unittest.TestCase):  # TEST SCENARIO
+
+    def setUp(self): 
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        
+    def test_a_success_login(self): 
+        # steps
+        browser = self.browser #buka web browser
+        browser.get("https://opensource-demo.orangehrmlive.com/ ") # buka situs
+        time.sleep(5)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input").send_keys("Admin") # isi email
+        time.sleep(2)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input").send_keys("admin123") # isi password
+        time.sleep(1)
+        browser.find_element(By.XPATH,"/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button").click() # klik tombol sign in
+        time.sleep(1)
+
+        # validasi
+        actualUrl = browser.current_url
+        expectedUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList"
+
+        # self.assertIn('Welcome', text_atas)
+        self.assertEqual(expectedUrl, actualUrl)
+
+    def tearDown(self): 
+        self.browser.close() 
+
+if __name__ == "__main__": 
+    unittest.main()
